c923fb2e66457110ff3b790ba7e4c0e287e0b19e
Gili G <60244395+Gili-G@users.noreply.github.com>
2020-08-16 01:22:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//make a on hover function to change text color of the links 
+
+function checkRadio() {
+    var correct = 0;
+    if (document.getElementById("a1").checked) {
+        correct += 1;
+    }
+    if (document.getElementById("c2").checked) {
+        correct += 1;
+    }
+    if (document.getElementById("a3").checked) {
+        correct += 1;
+    }
+    if (document.getElementById("b4").checked) {
+        correct += 1;
+    }
+    if (document.getElementById("c5").checked) {
+        correct += 1;
+    }
+}
+
+
